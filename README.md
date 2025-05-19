63e439f8eba6cdb3938fc517b0e1f35694ca5fc0
Giovanni D'Introno <giovanni.dintrono@gmail.com>
2025-05-19 08:46:31 +0000

    adding log in spring boot starter

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
-CAP with Java Project:
+<h1>CAP with Java Project:</h1>
 * Run archetype: *mvn archetype:generate -DarchetypeGroupId=com.sap.cds -DarchetypeArtifactId=cds-services-archetype -DarchetypeVersion=RELEASE*
 * add CF support: *mvn com.sap.cds:cds-maven-plugin:add -Dfeature=CF*
 * npm install
 * npm add -g @sap/cds-dk
 * npm ci
+* run with:  *mvn spring-boot:run
+* optional: insert kotlin support: add dependency, kotlin version and plugin in maven pom
+
  
 
--- srv/src/main/java/com/sap/gyo/Application.kt
+++ srv/src/main/java/com/sap/gyo/Application.kt
@@ -1,12 +1,17 @@
 package com.sap.gyo;
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 open class Application
+private val logger = LoggerFactory.getLogger(Application::class.java)
 
-fun main(args: Array<String>) {
-SpringApplication.run(Application::class.java, *args)
+fun main(args: Array<String>) 
+{
+    SpringApplication.run(Application::class.java, *args)
+    logger.info("Starting Application in Kotlin...")
 }
 
 
